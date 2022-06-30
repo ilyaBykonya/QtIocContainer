762f83a2f103012da61673fd3297418b92af4fec
ilyaBykonya <illya.aleksandrowich@mail.ru>
2022-06-30 12:15:19 +0300

    Repair example

--- Examples/CreateExample/main.cpp
+++ Examples/CreateExample/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QCoreApplication app(argc, argv);
     QtIoc::IocContainer::instance()->store_dependency("timer", std::make_shared<QtIoc::SingletonContainer>(new QTimer));
 
-    auto service = QtIoc::InstanceCreator{}.create<CreatableService, QPointer<QObject>>({ "timer" });
+    auto service = QtIoc::InstanceCreator{}.create<CreatableService, QPointer<QTimer>>({ "timer" });
     QtIoc::DependencyLoader<QTimer>{ "timer" }->start(500);
 
     return app.exec();
